8b1e703adbb209898b353c7163c231b752c599a7
cuidadoso <alexander_s@front.ru>
2017-11-16 15:44:16 +0300

    Migrate to ES6

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 /**
  * @author Alexander Pyreev
  */
- 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <p>hello react!</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Main/>,
+    document.getElementById("example")
+);
